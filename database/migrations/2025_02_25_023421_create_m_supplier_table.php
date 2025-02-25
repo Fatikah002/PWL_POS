0ad551a8d896d3580ad4574574238c7a7008ebd8
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-02-25 09:50:11 +0700

    P3_Praktikum 2.1_Langkah 8 (2)

--- database/migrations/2025_02_25_023421_create_m_supplier_table.php
+++ database/migrations/2025_02_25_023421_create_m_supplier_table.php
@@ -12,8 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('m_supplier', function (Blueprint $table) {
-            $table->id();
+            $table->id('supplier_id');
+            $table->string('supplier_kode', 10);
+            $table->string('supplier_nama', 100);
+            $table->string('supplier_alamat', 255);
             $table->timestamps();
+
         });
     }
 
