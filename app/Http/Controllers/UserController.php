35d5a9a2e25e6656e9f1954386a730e9e2b8e0e0
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-02-25 19:49:09 +0700

    P3_Praktikum 6_Langkah 10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id' => 4
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        $data=[
+            'nama' => 'Pelanggan Pertama',
         ];
         // UserModel::insert($data); //tambahkan data ke tabel user
         UserModel::where('username', 'customer-1')->update($data);
