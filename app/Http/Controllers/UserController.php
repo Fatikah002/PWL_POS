c81ff1852c0ae52f19655f5eb8980ee907459d29
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-02-25 19:45:42 +0700

    P3_Praktikum 6_Langkah 10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,8 +16,8 @@ class UserController extends Controller
             'password' => Hash::make('12345'),
             'level_id' => 4
         ];
-        UserModel::insert($data); //tambahkan data ke tabel user
-        
+        // UserModel::insert($data); //tambahkan data ke tabel user
+        UserModel::where('username', 'customer-1')->update($data);
         // coba akses model UserModel
         $user = UserModel::all();
         return view('user', ['data' => $user]);
