c4284288a4f2a97d8ddb8c6e3c038232d110ca39
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-03-03 20:23:02 +0700

    P4_Praktikum 2.1_Langkah 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,15 +16,15 @@ class UserController extends Controller
         //     'password' => Hash::make('12345'),
         //     'level_id' => 4
         // ];
-        $data=[
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data=[
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
         
-        $user = UserModel::all();
+        $user = UserModel::find(1);
         return view('user', ['data' => $user]);
         // // UserModel::insert($data); //tambahkan data ke tabel user
         // UserModel::where('username', 'customer-1')->update($data);
