20d47ceced93a81118f552869692e0f71547aa3c
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-03-04 09:36:56 +0700

    P4_Praktikum 2.6_Langkah 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,25 +11,11 @@ class UserController extends Controller
 {
         public function index()
         {
-               $user = UserModel::all();
-               return view('user', ['data' => $user]);
+                $user = UserModel::all();
+                return view('user', ['data' => $user]);
+        }
 
-                // dd($user);
-                // $data = [
-                //     'username' => 'customer-1',
-                //     'nama' => 'Pelanggan',
-                //     'password' => Hash::make('12345'),
-                //     'level_id' => 4
-                // ];
-                // $data=[
-                //     'level_id' => 2,
-                //     'username' => 'manager_tiga',
-                //     'nama' => 'Manager 3',
-                //     'password' => Hash::make('12345')
-                // ];
-                // UserModel::create($data);
-
-                // $user = UserModel::firstWhere('level_id', 1);
-
+        public function tambah(){
+                return view('user_tambah');
         }
 }
