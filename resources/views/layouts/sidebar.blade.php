c257ccd25b2229207433c321c40c69fefce68ace
Fatikah Salsabilla <sfatikah2005@gmail.com>
2025-03-25 20:34:19 +0700

    P7_Praktikum 1

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -65,6 +65,13 @@
                   <p> Data Supplier</p>
               </a>
           </li>
+          <li class="nav-header">Keluar dari Sistem</li>
+          <li class="nav-item">
+            <a href="{{ url('/logout') }}" class="nav-link {{ $activeMenu == '' ? 'active' : '' }} ">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p> Logout </p>
+            </a>
+        </li>
         </ul>
     </nav>
 </div>
